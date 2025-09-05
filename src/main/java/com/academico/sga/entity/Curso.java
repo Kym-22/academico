39384527ab74921124178f8f975df78e74c5dfad
Kym-22 <160817916+Kym-22@users.noreply.github.com>
2025-09-05 16:32:50 -0600

    Curso.java

--- src/main/java/com/academico/sga/entity/Curso.java
+++ src/main/java/com/academico/sga/entity/Curso.java
@@ -1,5 +1,38 @@
 package com.academico.sga.entity;
 
-public class Curso {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "cursos")
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private Integer semestre;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profesor_id")
+    private Profesor profesor;
+
+    public Curso() {}
+
+    public Curso(String nombre, Integer semestre, Profesor profesor) {
+        this.nombre = nombre;
+        this.semestre = semestre;
+        this.profesor = profesor;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public Integer getSemestre() { return semestre; }
+    public void setSemestre(Integer semestre) { this.semestre = semestre; }
+    public Profesor getProfesor() { return profesor; }
+    public void setProfesor(Profesor profesor) { this.profesor = profesor; }
 }
