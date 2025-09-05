7dceb3a637d78d2a79b6d54e401658e56978b6ca
Kym-22 <160817916+Kym-22@users.noreply.github.com>
2025-09-05 16:35:49 -0600

    ProfesorRepository.java

--- src/main/java/com/academico/sga/repository/ProfesorRepository.java
+++ src/main/java/com/academico/sga/repository/ProfesorRepository.java
@@ -1,5 +1,20 @@
 package com.academico.sga.repository;
 
-public class ProfesorRepository {
+import com.academico.sga.dto.ReporteCursosPorProfesorDTO;
+import com.academico.sga.entity.Profesor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
+public interface ProfesorRepository extends JpaRepository<Profesor, Long> {
+
+    List<Profesor> findByEspecialidadIgnoreCase(String especialidad);
+
+    List<Profesor> findByAniosExperienciaGreaterThanEqual(Integer min);
+
+
+    @Query("SELECT new com.academico.sga.dto.ReporteCursosPorProfesorDTO(p.nombre, COUNT(c)) " +
+           "FROM Profesor p LEFT JOIN p.cursos c GROUP BY p.nombre")
+    List<ReporteCursosPorProfesorDTO> contarCursosPorProfesor();
 }
