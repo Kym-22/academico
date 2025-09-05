a5d2a15b3ecf06289e0a8d97f33f47986b652080
Kym-22 <160817916+Kym-22@users.noreply.github.com>
2025-09-05 16:34:49 -0600

    CursoRepository.java

--- src/main/java/com/academico/sga/repository/CursoRepository.java
+++ src/main/java/com/academico/sga/repository/CursoRepository.java
@@ -1,5 +1,27 @@
 package com.academico.sga.repository;
 
-public class CursoRepository {
+import com.academico.sga.dto.ReportePromedioCursoDTO;
+import com.academico.sga.dto.ReporteTopCursosDTO;
+import com.academico.sga.entity.Curso;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+    List<Curso> findBySemestre(Integer semestre);
+
+    List<Curso> findByNombreContainingIgnoreCase(String nombre);
+
+    List<Curso> findByProfesor_NombreContainingIgnoreCase(String profesorNombre);
+
+    @Query("SELECT new com.academico.sga.dto.ReportePromedioCursoDTO(c.nombre, AVG(i.nota)) " +
+           "FROM Curso c JOIN c.inscripciones i GROUP BY c.nombre")
+    List<ReportePromedioCursoDTO> promedioNotasPorCurso();
+
+    @Query("SELECT new com.academico.sga.dto.ReporteTopCursosDTO(c.nombre, AVG(i.nota)) " +
+           "FROM Curso c JOIN c.inscripciones i GROUP BY c.nombre ORDER BY AVG(i.nota) DESC")
+    List<ReporteTopCursosDTO> top3Cursos(Pageable pageable);
 }
