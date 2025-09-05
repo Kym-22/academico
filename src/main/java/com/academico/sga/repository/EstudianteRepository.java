bf7a154ad4789b7e1214ed3c58e9c193446875ee
Kym-22 <160817916+Kym-22@users.noreply.github.com>
2025-09-05 16:35:14 -0600

    EstudianteRepository.java

--- src/main/java/com/academico/sga/repository/EstudianteRepository.java
+++ src/main/java/com/academico/sga/repository/EstudianteRepository.java
@@ -1,5 +1,18 @@
 package com.academico.sga.repository;
 
-public class EstudianteRepository {
+import com.academico.sga.entity.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
+
+    List<Estudiante> findByFechaNacimientoBefore(LocalDate end);
+
+    List<Estudiante> findByFechaNacimientoAfter(LocalDate start);
+
+    List<Estudiante> findByApellidoContainingIgnoreCase(String apellido);
+
+    List<Estudiante> findByFechaNacimientoBetween(LocalDate start, LocalDate end);
 }
